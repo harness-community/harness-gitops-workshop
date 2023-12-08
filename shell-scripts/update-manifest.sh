28f03e634bba216c91a7f709d1f0fce7e94ebd06
Dewan Ahmed <dewan.ishtiaque@hotmail.com>
2023-12-08 16:19:52 -0400

    Adding script to update deployment file with new image tag.

--- shell-scripts/update-manifest.sh
+++ shell-scripts/update-manifest.sh
@@ -1,7 +1,3 @@
-rm -rf harness-gitops-workshop
-git config --global user.email ci-bot@argocd.com && git config --global user.name ci-bot
-echo "cloning repo..."
-git clone https://oauth2:<+secrets.getValue("gh-pat")>@github.com/dewandemo/pharness-gitops-workshop.git
 cd podinfo
 echo "checkout source branch"
 git checkout master
@@ -9,3 +5,21 @@ echo "updating image tag in values file"
 sed -i "s,tag:.*,tag:\ <+trigger.commitSha>," charts/podinfo/values.yaml
 git add . && git commit -m "update image tag"
 git push
+
+export IMAGE_TAG=dewandemo/podinfo/cc41f26197249153820b3971fbb5f21f6d3e3c3b
+rm -rf harness-gitops-workshop
+git config --global user.email ci-bot@argocd.com && git config --global user.name ci-bot
+echo "cloning repo..."
+git clone https://oauth2:<+secrets.getValue("gh-pat")>@github.com/dewan-ahmed/harness-gitops-workshop.git
+cd harness-gitops-workshop
+FILE_PATH="configs/git-generator-files-discovery/apps/podinfo/deployment.yaml"
+
+# Detect OS and set the sed in-place edit command accordingly
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    SED_COMMAND="sed -i ''"
+else
+    SED_COMMAND="sed -i"
+fi
+
+echo "Updating image tag in deployment YAML"
+$SED_COMMAND "s|image: .*|image: $IMAGE_TAG|g" "$FILE_PATH"
